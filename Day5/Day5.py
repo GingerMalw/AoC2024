a4505b21790e146ecbcfbfea1e9caed8bb3298d0
Malwina <malwina.wesola@gmail.com>
2025-06-06 17:02:35 +0200

    done day5, updated functions for part 1 and cleared the code

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -1,8 +1,12 @@
+# Day 5: Print Queue
+
+# Data inputs:
 # updates = r"C:\Users\malwi\Desktop\Codding_stuff_MW\AdventOfCode2024\AoC2024\Day5\Day5_sample_input_update.txt"
 updates = r"C:\Users\malwi\Desktop\Codding_stuff_MW\AdventOfCode2024\AoC2024\Day5\Day5_input_update.txt"
 # ordering = r"C:\Users\malwi\Desktop\Codding_stuff_MW\AdventOfCode2024\AoC2024\Day5\Day5_sample_input_ordering.txt"
 ordering = r"C:\Users\malwi\Desktop\Codding_stuff_MW\AdventOfCode2024\AoC2024\Day5\Day5_input_ordering.txt"
 
+# Part One function:
 def find_correct_updates(all_updates, all_orders):
     correct_updates = []
     ab_order = {}
@@ -24,7 +28,7 @@ def find_correct_updates(all_updates, all_orders):
                 # print(f"  * current_num: {current_num}, previous: {previous_num}, seconds: {second_num}")
                 for num in previous_num:
                     if num in second_num:
-                        # print(f"    ! KOLIZJA: {num} w seconds dla {current_num}") 
+                        # print(f" ISSUE: {num} w seconds for {current_num}") 
                         valid = False
                         break
                 
@@ -45,7 +49,47 @@ def take_middle(updates_list):
         middle_numbers.append(line[middle_number])
         
     # print(f"Middle numbers are: {middle_numbers}")
-    print(f"Sum for middle numbers is: {sum(middle_numbers)}")
+    return sum(middle_numbers)
+
+# Function upgraded for Part Two:
+def page_ordering_rules(list, all_orders):
+    fix_updates = []
+    ab_order = {}
+    for order in all_orders:
+        first = order[0]
+        second = order[1]
+        if first not in ab_order:
+            ab_order[first] = set()
+        ab_order[first].add(second)
+    
+    for update in list:
+        valid = True
+        # print(f"UPDATE: {update}")
+
+        for idx, current_num in enumerate(update):
+            if current_num in ab_order:
+                previous_num = update[:idx]
+                second_num = ab_order[current_num]
+                # print(f"  * current_num: {current_num}, previous: {previous_num}, seconds: {second_num}")
+                for num in previous_num:
+                    if num in second_num:
+                        # print(f" ISSUE: {num} w seconds for {current_num}") 
+                        update[idx] , update[idx - 1] = update[idx - 1], update[idx]
+                        # print(f"NEW: {update}")
+                        list.append(update)
+                        valid = False
+                        break
+
+                if not valid:
+                    break
+
+        if valid:
+            fix_updates.append(update)
+    
+    return fix_updates
+
+
+# --- Part One ---
 
 # extracting updates
 with open(updates, 'r', encoding="utf-8") as plik:
@@ -71,4 +115,26 @@ for order in orders:
 correct_update = find_correct_updates(all_updates, all_orders)
 # print(correct_update)
     
-take_middle(correct_update)
+middle_part1 = take_middle(correct_update)
+print(f"The mid values sum for Part One: {middle_part1}")
+
+
+# --- Part Two ---
+
+incorrect_updates = []
+for update in all_updates:
+    if update not in correct_update:
+        incorrect_updates.append(update)
+# print(incorrect_updates)
+
+# !! Well, the permutation for this amount of data will kill the process :) !!
+# for update in incorrect_updates:
+#     update_perm = permutations(update)
+#     for perm in update_perm:
+#         incorrect_updates_fix.append(find_correct_updates(perm, all_orders))
+
+incorrect_updates_fix = page_ordering_rules(incorrect_updates, all_orders)
+# print(incorrect_updates_fix)
+
+middle_part2 = take_middle(incorrect_updates_fix)
+print(f"The mid values sum for Part Two: {middle_part2}")
